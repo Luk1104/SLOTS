95aa7652dba6180c856b8b5a41c294d0c9f6ed4f
lukasz <luk@luk.tail748726.ts.net>
2025-10-25 13:11:14 +0200

    spin algortihm, return balance

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ import os
 import sys
 import time
 import jwt
+import secrets
+import string
 
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
@@ -72,6 +74,85 @@ def login():
         return jsonify({'token': token, 'balance': balance}), 200
 
     return jsonify({'message': 'Invalid credentials'}), 401
+
+#-------------------------------------------------------------------
+
+@app.route('/api/spin', methods=['POST'])
+def spin():
+
+    data = request.get_json() or {}
+    token = data.get('token')
+    payload = jwt.decode(token, jwt_secret, algorithm=jwt_algorithm)
+    #print(payload)
+
+    email = payload['sub']
+    bet = payload['bet']
+
+    user = users_collection.find_one({'email': email})
+    balance = user.get('balance')
+
+    if bet > balance:
+        return jsonify({'message':'Inssuficient balance'}), 400
+    
+    #------------------------------------------ Losowanie
+
+    alphabet = string.digits
+    secretsGenerator = secrets.SystemRandom()
+    output = secretsGenerator.sample(alphabet,3) #mamy liczbe od 0 do 999
+
+    win_list = [0,111,222,333]
+
+    result = ""
+    for i in output:
+        result += i
+
+    result_int = int(result)
+
+    if result_int <= 999 and result_int >= 980: # 2% na wina x20
+        result = "333"
+    
+    elif result_int <= 691 and result_int >= 641: # 5% na wina x5
+        result = "222"
+
+    elif result_int <= 383 and result_int >= 283: # 10% na wina x2
+        result = "111"
+
+    elif result_int <= 199 and result_int >= 0: # 20% na wina x1.5
+        result = "000"
+
+    else:
+
+        list = []
+
+        for i in result:
+            list.append(int(i)%3)
+
+        #print(list)
+
+        if list[0] == list[1] and list[1] == list[2]:
+            list[2] += 1
+
+        result = ""
+
+        for i in list:
+            result += str(i)
+        
+
+    #print(result_int)
+    #print(result)
+
+    if result == "000":
+        win_multiplier = 1.5
+    elif result == "111":
+        win_multiplier = 2
+    elif result == "222":
+        win_multiplier = 5
+    elif result == "333":
+        win_multiplier = 20
+
+    balance += bet*win_multiplier
+
+    return jsonify({'result':result , 'balance':balance}), 200
 
 @app.route('/', methods=['GET'])
 def main():
