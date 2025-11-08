c21c16bd81c11dc43fbf685503574dd9ee0a5d2c
Luk1104 <146875463+Luk1104@users.noreply.github.com>
2025-11-08 11:16:42 +0100

    Disable debug mode in app.run

--- backend/app.py
+++ backend/app.py
@@ -233,4 +233,4 @@ def main():
     return jsonify({'message': 'API running'}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
